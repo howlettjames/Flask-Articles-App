cbf396e5952944365c85b8de7c9d9d5691641986
howlettjames <jamesf6888@gmail.com>
2020-11-01 20:00:56 -0600

    first commit

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'lorem',
+            'author': 'Jaime Bastida',
+            'create_date': '04-25-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'lorem',
+            'author': 'Jaime Bastida',
+            'create_date': '04-25-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'lorem',
+            'author': 'Jaime Bastida',
+            'create_date': '04-25-2017'
+        },
+    ]
+
+    return articles
